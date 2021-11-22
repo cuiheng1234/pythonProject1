db2e7becd05f9ce04ed07e3d7994a9b66a03cbcb
cuiheng <2297819080@qq.com>
2021-11-22 16:48:32 +0800

    以后的模板

--- tensorboard1.py
+++ tensorboard1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tensorboardX import SummaryWriter
-
+# 以后的模板
 writer = SummaryWriter(comment='base_scalar')
 for epoch in range(100):
     writer.add_scalar('scalar/test', np.random.rand(), epoch)
